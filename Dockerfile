d16d3cafc25cb3d2d28980580885caa757d3a429
JulesAaelio <jules.laurent@outlook.com>
2022-05-26 17:41:07 +0200

    Setup SQLAlchemy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.7-alpine
 WORKDIR /app
 ENV FLASK_RUN_HOST=0.0.0.0
-
+RUN apk add gcc musl-dev
+RUN apk add --update mariadb-dev
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 
--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ':////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://toto:tata@db:3306/titi'
 db = SQLAlchemy(app)
+
+## Data model
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), unique=True, nullable=False)
+    lastname = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
 
 @app.route("/")
 def hello_world():
