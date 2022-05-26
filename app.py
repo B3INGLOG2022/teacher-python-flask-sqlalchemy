b43bda48c21585fd1c1fa5fa161228af2fa500f7
JulesAaelio <jules.laurent@outlook.com>
2022-05-26 18:29:11 +0200

    Add endpoint to get all students

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from flask import request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://toto:tata@db:3306/titi'
@@ -7,10 +10,10 @@ db = SQLAlchemy(app)
 
 ## Data model
 
-class Student(db.Model):
+class Student(db.Model,SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(80), unique=True, nullable=False)
-    lastname = db.Column(db.String(80), unique=True, nullable=False)
+    firstname = db.Column(db.String(80), unique=False, nullable=False)
+    lastname = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
@@ -23,6 +26,7 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-# @app.route("/students")
-# def get_students():
-#     return Student.query.all()
+@app.route("/students")
+def get_students():
+    students = Student.query.all()
+    return jsonify([student.to_dict() for student in students])
